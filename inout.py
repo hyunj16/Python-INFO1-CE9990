7c1f7bb022dd6c93ce67e0d1a5fc89f2589867f4
hyunj16 <hyunj16@gmail.com>
2017-06-22 08:03:05 -0400

    Update inout.py

--- inout.py
+++ inout.py
@@ -10,13 +10,12 @@ while choice == "y":
         input1 = input("What is your hourly rate?\t$ ")
     except EOFError:
         sys.exit(0)
-
     try:
         hourlyrate = float(input1)
     except ValueError:
         print("You must be an unpaid-intern. $0 / hour")
         hourlyrate = 0
-    print(hourlyrate)
+    #print(hourlyrate)
 
     try:
         input2 = input("How many hours a day do you work?\t")
@@ -27,31 +26,31 @@ while choice == "y":
     except ValueError:
         print("I will assume 0 hours / day.")
         hoursaday = 0
-    print(hoursaday)
+    #print(hoursaday)
     
     try:
         input3 = input("How many days do you work in a week?\t")
     except EOFError:
         sys.exit(0)
-
     try:
         daysaweek = int(input3)
     except ValueError:
         print("I guess you don't work.")
         daysaweek = 1
-    print(daysaweek)
+    #print(daysaweek)
     
     weeksinayear = 52
 
     salary = hourlyrate * daysaweek * hoursaday * weeksinayear
-    print(salary)
+    #print(salary)
     salaryinK = salary/1000
-    print(salaryinK)
-    salaryinthousands = int(salaryinK)
+    #print(salaryinK)
+    salaryinthousands = int(round(salaryinK))
     
-
+    print()
     print("Your salary is about ", "$ ", salaryinthousands, "K." , sep="")
-
+    print()
+    
     try:
         input4 = input("Would you want to calculate again? Press \"Y\" to continue. ")
     except EOFError:
