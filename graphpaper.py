960e1a8ce47cd9cac36d373f6b897bd231aac075
hyunj16 <hyunj16@gmail.com>
2017-06-23 18:53:45 -0400

    Create graphpaper.py

---
+++ graphpaper.py
@@ -0,0 +1,51 @@
+import sys
+
+r = int(7)#int(input("How many rows of boxes?\t"))
+c = int(5)#int(input("How many columns of boxes?\t"))
+rb = int(4)#int(input("How many rows of blanks in each boxes?\t"))
+cb = int(3)#int(input("How many columns of blanks in each boxes?\t"))
+
+a = str("+")
+b = str("-")
+d = str("|")
+e = str(" ")
+
+h = 1
+while h <= c:
+    
+    j = 1
+    while j <= r:
+        print(a, end="")
+
+        k = 1
+        while k <= cb:
+            print(b, end="")
+            k += 1
+
+        j += 1
+    print(a)
+
+    l = 1
+    while l <= rb:
+        i = 1
+        while i <= r:
+            print(d, end="")
+            print(cb*e, end="")
+            i += 1
+        print(d)
+        l += 1
+    
+    h += 1
+
+m = 1
+while m <= r:
+    print(a, end="")
+
+    n = 1
+    while n <= cb:
+        print(b, end="")
+        n += 1
+    m += 1
+print(a)
+    
+sys.exit(0)
