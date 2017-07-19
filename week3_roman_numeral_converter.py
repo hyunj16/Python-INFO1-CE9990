5ff8ab53df65ff621d9470d5c3296b19edd6c8d2
hyunj16 <hyunj16@gmail.com>
2017-07-18 23:24:22 -0400

    Update week3_roman_numeral_converter.py

--- week3_roman_numeral_converter.py
+++ week3_roman_numeral_converter.py
@@ -4,6 +4,8 @@ week3_year_in_roman_numeral.py
 Tried to come up with different method to convert a number in Roman numerals using "while" loop and manipulating strings.
 """
 
+import sys
+
 def opening():
     print()
     print("Welcome to Roman Numeral Converter")
@@ -28,57 +30,56 @@ def getInt(ask):
             continue
         return i
 
-def calculation(number, roman):
-    while number > 0:
-        print()
-        while number >= 1000:
-            number -= 1000
-            roman = roman + "M"
-            #printoutcome(year, roman)
-        while number >= 900:
-            number -= 900
-            roman = roman + "CM"
-            #printoutcome(year, roman)
-        while number >= 500:
-            number -= 500
-            roman = roman + "D"
-            #printoutcome(year, roman)
-        while number >= 400:
-            number -= 400
-            roman = roman + "CD"
-            #printoutcome(year, roman)
-        while number >= 100:
-            number -= 100
-            roman = roman + "C"
-            #printoutcome(year, roman)
-        while number >= 90:
-            number -= 90
-            roman = roman + "XC"
-            #printoutcome(year, roman)
-        while number >= 50:
-            number -= 50
-            roman = roman + "L"
-            #printoutcome(year, roman)
-        while number >= 40:
-            number -= 40
-            roman = roman + "XL"
-            #printoutcome(year, roman)
-        while number >= 10:
-            number -= 10
-            roman = roman + "X"
-            #printoutcome(year, roman)
-        while number >= 5:
-            number -= 5
-            roman = roman + "V"
-            #printoutcome(year, roman)
-        while number >= 4:
-            number -= 4
-            roman = roman + "IV"
-            #printoutcome(year, roman)
-        while number >= 1:
-            number -= 1
-            roman = roman + "I"
-            #printoutcome(year, roman)
+def calculation(number):
+    roman = ""
+    while number >= 1000:
+        number -= 1000
+        roman += "M"
+        #printoutcome(year, roman)
+    while number >= 900:
+        number -= 900
+        roman += "CM"
+        #printoutcome(year, roman)
+    while number >= 500:
+        number -= 500
+        roman += "D"
+        #printoutcome(year, roman)
+    while number >= 400:
+        number -= 400
+        roman += "CD"
+        #printoutcome(year, roman)
+    while number >= 100:
+        number -= 100
+        roman += "C"
+        #printoutcome(year, roman)
+    while number >= 90:
+        number -= 90
+        roman += "XC"
+        #printoutcome(year, roman)
+    while number >= 50:
+        number -= 50
+        roman += "L"
+        #printoutcome(year, roman)
+    while number >= 40:
+        number -= 40
+        roman += "XL"
+        #printoutcome(year, roman)
+    while number >= 10:
+        number -= 10
+        roman += "X"
+        #printoutcome(year, roman)
+    while number >= 5:
+        number -= 5
+        roman += "V"
+        #printoutcome(year, roman)
+    while number >= 4:
+        number -= 4
+        roman += "IV"
+        #printoutcome(year, roman)
+    while number >= 1:
+        number -= 1
+        roman += "I"
+        #printoutcome(year, roman)
     return(roman)
     
 def results(c, d):
@@ -98,10 +99,11 @@ if inputnumber > 4000 or inputnumber <= 0:
 else:
     initialroman = ""
     initialnumber = inputnumber
-    initialroman
 
     #Processing Roman Numeral
-    romanoutput = calculation(inputnumber, initialroman)
+    romanoutput = calculation(inputnumber)
 
 #Print Outcome
 results(initialnumber, romanoutput)
+
+sys.exit(0)
