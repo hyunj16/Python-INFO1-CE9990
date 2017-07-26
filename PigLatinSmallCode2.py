8266f022cb49c0f44898fe6da15dcb240e6f7c1e
hyunj16 <hyunj16@gmail.com>
2017-07-26 17:12:29 -0400

    Update PigLatinSmallCode2.py

--- PigLatinSmallCode2.py
+++ PigLatinSmallCode2.py
@@ -5,12 +5,13 @@ Testing
 """
 import sys
 
-s = "strictly"
+s = "today"
 
 def findmultipleconsonant(wordinput2):
     i = 0
     wordlength = len(wordinput2)
-    while i < wordlength:
+    check = 0
+    while i < wordlength and check == 0:
         print(wordinput2[i])
         n = 0
         currentword = wordinput2[i]
@@ -23,13 +24,14 @@ def findmultipleconsonant(wordinput2):
                 "u"
             ]
             vowel = vowels[n]
-            #print(vowel)
-            if wordinput2[i] == vowel:
+            print(vowel)
+            if currentword == vowel:
                 print("FOUND IT.")
+                check = 1
                 break
             n += 1
         i += 1
-    return i
+    return i-1
 
 testing = findmultipleconsonant(s)
 print(testing)
