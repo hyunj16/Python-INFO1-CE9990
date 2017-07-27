824cca799655f0dec15ae8af8807c49a6bc2428e
hyunj16 <hyunj16@gmail.com>
2017-07-27 08:11:44 -0400

    Update PigLatinSmallCode2.py

--- PigLatinSmallCode2.py
+++ PigLatinSmallCode2.py
@@ -1,32 +1,17 @@
-
 import sys
 
 s = "today"
 
 def findmultipleconsonant(wordinput2):
-    i = 0
-    wordlength = len(wordinput2)
-    while i < wordlength:
-        print(wordinput2[i])
-        n = 0
-        currentword = wordinput2[i]
-        while n < 5:
-            vowels = [
-                "a",
-                "e",
-                "i",
-                "o",
-                "u"
-            ]
-            vowel = vowels[n]
-            if currentword == vowel:
-                print("FOUND IT.")
-                return i   #instead of the break
-            n += 1
-        i += 1
+    for i, c in enumerate(wordinput2):
+        print(c)
+        if c in "aeiou":
+            print("FOUND IT.")
+            return i
     print("The word contains no vowels.")
     return -1
 
 testing = findmultipleconsonant(s)
 print(testing)
+
 sys.exit(0)
